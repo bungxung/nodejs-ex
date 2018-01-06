d1eedadd9ff77c68263f7c828ead586f907f2748
bungxung@yahoo.de <bungxung@yahoo.de>
2018-01-06 17:39:48 +0100

    changed structrue

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-var express = require('express');
-var app = express();
-var db = require('./db');
-var morgan = require('morgan');
+//  OpenShift sample Node application
+var express = require('express'),
+    app = express(),
+    morgan = require('morgan');
 
 Object.assign = require('object-assign')
 
@@ -33,11 +33,89 @@ if (mongoURL == null && process.env.DATABASE_SERVICE_NAME) {
     }
 }
 
+var db = null,
+    dbDetails = new Object();
+
+var initDb = function(callback) {
+    if (mongoURL == null) return;
+
+    var mongodb = require('mongodb');
+    if (mongodb == null) return;
+
+    mongodb.connect(mongoURL, function(err, conn) {
+        if (err) {
+            callback(err);
+            return;
+        }
+
+        db = conn;
+        dbDetails.databaseName = db.databaseName;
+        dbDetails.url = mongoURLLabel;
+        dbDetails.type = 'MongoDB';
+
+        console.log('Connected to MongoDB at: %s', mongoURL);
+    });
+};
+
+app.get('/', function(req, res) {
+    // try to initialize the db on every request if it's not already
+    // initialized.
+    if (!db) {
+        initDb(function(err) {});
+    }
+    if (db) {
+        var col = db.collection('counts');
+        // Create a document with request IP and current time of request
+        col.insert({ ip: req.ip, date: Date.now() });
+        col.count(function(err, count) {
+            if (err) {
+                console.log('Error running count. Message:\n' + err);
+            }
+            res.render('index.html', { pageCountMessage: count, dbInfo: dbDetails });
+        });
+    } else {
+        res.render('index.html', { pageCountMessage: null });
+    }
+});
+
+app.get('/pagecount', function(req, res) {
+    // try to initialize the db on every request if it's not already
+    // initialized.
+    if (!db) {
+        initDb(function(err) {});
+    }
+    if (db) {
+        db.collection('counts').count(function(err, count) {
+            res.send('{ pageCount: ' + count + '}');
+        });
+    } else {
+        res.send('{ pageCount: -1 }');
+    }
+});
+
+app.get('/card', function(req, res) {
+    // try to initialize the db on every request if it's not already
+    // initialized.
+    if (!db) {
+        initDb(function(err) {});
+    }
+    if (db) {
+        db.collection('products').insert({ item: "card", qty: 15 });
+    } else {
+        //res.send('{ pageCount: -1 }');
+    }
+});
+
+
+
+var db = require('./db');
+
 var UserController = require('./user/UserController');
 app.use('/users', UserController);
 
 var ProductController = require('./product/ProductController');
 app.use('/products', ProductController);
+
 
 
 // error handling
@@ -46,6 +124,9 @@ app.use(function(err, req, res, next) {
     res.status(500).send('Something bad happened!');
 });
 
+initDb(function(err) {
+    console.log('Error connecting to Mongo. Message:\n' + err);
+});
 
 app.listen(port, ip);
 console.log('Server running on http://%s:%s', ip, port);
