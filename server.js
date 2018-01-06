1ae7fc3b3ed8fd76a37d2ad0226dc8a2ab4672a6
bungxung@yahoo.de <bungxung@yahoo.de>
2018-01-06 17:27:38 +0100

    test

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ var ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 var server = app.listen(port, ip);
 console.log('Server running on http://%s:%s', ip, port);
 
-
+module.exports = app;
 //app.listen(port, ip);
 //console.log('Server running on http://%s:%s', ip, port);
