0eaf4fecf8eaef914b695e920b0f2b8c3c1852d5
bungxung@yahoo.de <bungxung@yahoo.de>
2018-01-06 17:15:41 +0100

    server.js changed

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ var app = require('./app');
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
 var ip = ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 //
-var server = app.listen(port, ip, function() {
-    console.log('Express server listening on port ' + port);
-});
+var server = app.listen(port, ip);
+console.log('-------------------------------------abc');
+console.log('Server running on http://%s:%s', ip, port);
 
 
 
