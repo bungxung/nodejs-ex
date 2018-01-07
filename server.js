350fdcb3d13d0505895102180ec3f2d434da5de9
bungxung@yahoo.de <bungxung@yahoo.de>
2018-01-07 16:30:44 +0100

    cors enabled

--- server.js
+++ server.js
@@ -2,6 +2,10 @@
 var express = require('express'),
     app = express();
 
+var cors = require('cors');
+
+app.use(cors());
+
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
@@ -23,12 +27,20 @@ app.get('/', function(req, res) {
 
 // error handling
 app.use(function(err, req, res, next) {
-    console.error(err.stack);
-    res.status(500).send('Something bad happened!');
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
 });
 
 
-app.listen(port, ip);
-console.log('Server running on http://%s:%s', ip, port);
+
+//app.listen(port, ip);
+//console.log('Server running on http://%s:%s', ip, port);
+
+
+app.listen(port, ip, function() {
+    console.log('Server running on http://%s:%s', ip, port);
+});
 
 module.exports = app;
